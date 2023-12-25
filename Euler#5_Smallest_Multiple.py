7dc1c7a4c5acd507cfb762e13216f68b3fa87c73
BabkinDG <denis_babkin@bk.ru>
2023-12-25 07:40:48 +0300

    update Euler#5

--- Euler#5_Smallest_Multiple.py
+++ Euler#5_Smallest_Multiple.py
@@ -1,17 +1,18 @@
 #Самое маленькое число, которое делится нацело на все числа от 1 до 20
 number = 0
 int_div = 0
+max_div = 20
 answer = False
 
 while answer == False:
-    number += 20
-    for divider in range(1, 21):
+    number += max_div
+    for divider in range(1, max_div + 1):
          if number % divider == 0:
             int_div += 1
-            if int_div == 20:
+            if int_div == max_div:
                 answer = True
          else:
             int_div = 0
             break
 
-print('Самым маленьким числом, которое делится нацело на все числа от 1 до 20 является ' + str(number))
+print('Самым маленьким числом, которое делится нацело на все числа от 1 до ' + str(max_div) + ' является ' + str(number))
