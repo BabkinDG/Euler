7db0d27f9675f251b60125071d7d2083b05e8c5a
BabkinDG <denis_babkin@bk.ru>
2024-03-05 22:52:39 +0300

    Euler#9 problem solved

---
+++ Euler#9_Special_Pythagorean_Triplet.py
@@ -0,0 +1,6 @@
+for a in range(1, 1000):
+    for b in range(1, 1000):
+        for c in range(1, 1000):
+            if a < b and b < c and (a + b + c == 1000) and (a**2 + b**2 == c**2):
+                print(f'{a*b*c} {a} {b} {c}')
+                break
