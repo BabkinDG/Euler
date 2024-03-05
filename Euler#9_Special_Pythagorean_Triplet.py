4278297ce54219243cadc34fe9441f1416b09f8f
BabkinDG <denis_babkin@bk.ru>
2024-03-05 23:05:00 +0300

    Euler#9 Reduced program running time

--- Euler#9_Special_Pythagorean_Triplet.py
+++ Euler#9_Special_Pythagorean_Triplet.py
@@ -1,8 +1,8 @@
 import sys
 
-for a in range(1, 1000):
-    for b in range(1, 1000):
-        for c in range(1, 1000):
+for a in range(1, 500):
+    for b in range(1, 500):
+        for c in range(1, 500):
             if a < b and b < c and (a + b + c == 1000) and (a**2 + b**2 == c**2):
                 print(f'{a*b*c} {a} {b} {c}')
                 sys.exit()
