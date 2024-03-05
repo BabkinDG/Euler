5e034ded34c27768a8afc5392c5c015be8da0670
BabkinDG <denis_babkin@bk.ru>
2024-03-05 23:11:20 +0300

    Euler#9 some update

--- Euler#9_Special_Pythagorean_Triplet.py
+++ Euler#9_Special_Pythagorean_Triplet.py
@@ -4,5 +4,6 @@ for a in range(1, 500):
     for b in range(1, 500):
         for c in range(1, 500):
             if a < b and b < c and (a + b + c == 1000) and (a**2 + b**2 == c**2):
-                print(f'{a*b*c} {a} {b} {c}')
+                result = a*b*c
+                print(f'{result} {a} {b} {c}')
                 sys.exit()
