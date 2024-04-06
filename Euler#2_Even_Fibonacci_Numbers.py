da79d5a188d2f44707dcf759b5eba54e15e33ed3
BabkinDG <denis_babkin@bk.ru>
2024-04-06 19:51:07 +0300

    add Euler#2 Even Fibonacci Numbers solution update

--- Euler#2_Even_Fibonacci_Numbers.py
+++ Euler#2_Even_Fibonacci_Numbers.py
@@ -1,16 +1,16 @@
 #Сумма четных членов последовательности Фибоначи до 4 000 000
 summ = 0
-Fnum = [1]
-Fnew = 0
+fnum = [1]
+fnew = 0
 range = 4000000
 
-while Fnew < range - 1:
-    Fnew = Fnum[len(Fnum) - 1] + Fnum[len(Fnum) - 2]
-    if Fnew < range:
-        Fnum.append(Fnew)
-    if (Fnew % 2) == 0 and Fnew < range:
-        summ += Fnew
+while fnew < range - 1:
+    fnew = fnum[len(fnum) - 1] + fnum[len(fnum) - 2]
+    if fnew < range:
+        fnum.append(fnew)
+    if (fnew % 2) == 0 and fnew < range:
+        summ += fnew
 
-print(Fnum)
+print(fnum)
 print(summ)
 print('Сумма четных членов последовательности Фибоначи до 4 000 000 равна ' + str(summ))
